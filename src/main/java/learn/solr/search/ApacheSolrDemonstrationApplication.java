25242c7c9ecd06a6ecba82658811d7c23f2ad48d
Pranesh J R <praneshjr6@gmail.com>
2022-03-15 19:44:33 +0530

    Imported Spring App from Spring Initializr

---
+++ src/main/java/learn/solr/search/ApacheSolrDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package learn.solr.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheSolrDemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheSolrDemonstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/solr/search/ApacheSolrDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.solr.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheSolrDemonstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
