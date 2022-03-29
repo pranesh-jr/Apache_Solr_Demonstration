ecd4326c0bfd9beaca63ae54c08a732013eac483
Pranesh J R <praneshjr6@gmail.com>
2022-03-29 20:55:07 +0530

    Implemented log4j2 logger through SLF4J

--- src/main/java/learn/solr/search/ServletInitializer.java
+++ src/main/java/learn/solr/search/ServletInitializer.java
@@ -1,12 +1,15 @@
 package learn.solr.search;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+	private static Logger logger = LoggerFactory.getLogger(ServletInitializer.class);
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		logger.debug("Initializing Servlet");
 		return application.sources(ApacheSolrDemonstrationApplication.class);
 	}
 
